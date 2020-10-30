afa09aa6c6cc2cb1ec14e7a379fb15f087677267
Kulkarni <pradnya.a.kulkarni@accenture.com>
2020-10-30 16:27:53 +0530

    moving elements done

--- js/matrix.js
+++ js/matrix.js
@@ -1,4 +1,4 @@
-const originalMatrix = [1,2,3,4,5,6,7,8,9,10,11,''];
+const originalMatrix = [1,2,3,4,5,6,7,8,9,10,11,0];
 const generatedMatrix = [];
 const totalNoOfElements = 12;
 const totalRows = 3;
@@ -18,69 +18,47 @@ function allowDrop(ev) {
 
 function drag(ev) {
     dragged = ev;
-    const movedEle = parseInt(ev.target.innerText);
-    if (elementsToMove.indexOf(movedEle) > -1) {
-        // ev.dataTransfer.setData("text", ev.target.id);
-    }
 }
 
 function drop(ev) {
-    ev.preventDefault();
-    // var data = ev.dataTransfer.getData("text");
-    ev.target.style.top = (ev.target.offsetTop - 2) + "px";
-    ev.target.style.left = (ev.target.offsetLeft - 0) + "px";
-    const origcloneNode = document.getElementById(dragged.target.id).cloneNode(true);
-    const empcloneNode = document.getElementById(ev.target.id).cloneNode(true);
-    ev.target.replaceWith(origcloneNode);
-    dragged.target.replaceWith(empcloneNode);
-    // ev.target.appendChild(document.getElementById(data));
-}
-
-function moveElement(ele) {
-    const elem = ele.target;
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    const movedEle = parseInt(ele.target.innerText);
-    if (elementsToMove.indexOf(movedEle) > -1) {
-        // const currentElementIndex = generatedMatrix.indexOf(movedEle);
-        // generatedMatrix[emptyElement] = movedEle;
-        // emptyElement = currentElementIndex;
-        // generatedMatrix[currentElementIndex] = '';
-        document.getElementById(movedEle + 'no').onmousedown = dragMouseDown;
-        console.log("after changing", generatedMatrix);
+    if (elementsToMove.indexOf(parseInt(dragged.target.innerText)) > -1) {
+        ev.preventDefault();
+        ev.target.style.top = (ev.target.offsetTop - 2) + "px";
+        ev.target.style.left = (ev.target.offsetLeft - 0) + "px";
+        const origcloneNode = document.getElementById(dragged.target.id).cloneNode(true);
+        origcloneNode.draggable = true;
+        origcloneNode.ondragstart = drag;
+        const empcloneNode = document.getElementById(ev.target.id).cloneNode(true);
+        empcloneNode.ondrop = drop;
+        empcloneNode.ondragover = allowDrop;
+        ev.target.replaceWith(origcloneNode);
+        dragged.target.replaceWith(empcloneNode);
+        restructureElements();
     }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-      }
-    
-      function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-      }
-    
-      function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elem.style.top = (elem.offsetTop - pos2) + "px";
-        elem.style.left = (elem.offsetLeft - pos1) + "px";
-        console.log('ele', elem);
-      }
 }
 
+function restructureElements() {
+    const children = document.getElementById('mainDiv').childNodes;
+    const childNodesLength = children.length;
+    const rows = [];
+    let nos = [];
+    for(let i=0; i<childNodesLength; i++) {
+        if(children[i] && children[i].id) {
+            const content = parseInt(children[i].textContent);
+            nos.push(content);
+            const itemsInRow = nos.length;
+            if(content === 0) {
+                indexEmptyRowEle = itemsInRow - 1;
+                rowIndexContainingEmpty = rows.length;
+            }
+            if(itemsInRow === noOfElementPerRow) {
+                rows.push(nos);
+                nos = [];
+            }
+        }
+    }
+    highlightNeighbouringElemets(rows);
+}
 
 
 function load() {
@@ -91,7 +69,6 @@ function load() {
         const pickedNo = sample();
         let divElement = document.createElement('div');
         divElement.className = 'noDiv';
-        // divElement.onclick = moveElement;
         divElement.innerHTML = pickedNo;
         divElement.id = pickedNo + 'no';
         const index = originalMatrix.indexOf(pickedNo);
@@ -103,7 +80,7 @@ function load() {
         } 
         divElement.draggable = true;
         divElement.ondragstart = drag;
-        if(pickedNo === '') {
+        if(pickedNo === 0) {
             indexEmptyRowEle = itemsInRow - 1;
             rowIndexContainingEmpty = rows.length;
             emptyElement = i;
@@ -129,5 +106,4 @@ function highlightNeighbouringElemets(rows) {
           }
       }
       elementsToMove = elements;
-      console.log('elements to move ', elementsToMove);
 }
